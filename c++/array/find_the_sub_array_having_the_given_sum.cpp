a2552527dc0468b1b980538c3f707d6953e4ea76
shinelikeasmile <49489706+shinelikeasmile@users.noreply.github.com>
2019-12-23 13:45:57 +0530

    Create find_the_sub_array_having_the_given_sum.cpp

---
+++ c++/array/find_the_sub_array_having_the_given_sum.cpp
@@ -0,0 +1,35 @@
+	#include <bits/stdc++.h>
+	using namespace std;
+	int main()
+	{
+		map<signed int,unsigned int>m;
+		vector<int>v;
+		int n;
+		cout<<"no.of elements ";
+		cin>>n;
+		int r;
+		cout<<"sum ";
+	    cin>>r;//given sum
+	    cout<<"enter elements ";
+		while(n--)
+		{
+			int temp;
+			cin>>temp;
+			v.push_back(temp);
+		}
+		cout<<endl;
+		signed int sum=0;
+		for(unsigned int i=0;i<v.size();i++)
+		{
+			sum+=v[i];
+			m[sum]=i;
+			if(sum==r){for(auto itr=v.begin();itr<=v.begin()+i;itr++)cout<<*itr<<" ";}
+			else if(m.find(sum-r)!=m.end())
+			{
+				for(auto itr=v.begin()+m.find(sum-r)->second+1;itr<=v.begin()+i;itr++)cout<<*itr<<" ";
+				break;			
+			}
+			else{}
+		}
+		
+	}
