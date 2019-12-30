67c959cb27db73bec5e65291ee600f2e76cd3ae0
shinelikeasmile <49489706+shinelikeasmile@users.noreply.github.com>
2019-12-30 16:49:46 +0530

    Create singly_linked_list.py

---
+++ python/linked_list/singly_linked_list.py
@@ -0,0 +1,20 @@
+#implementation of linked list
+class Node:
+	def __init__(self,data):
+		self.data=data
+		self.next=None
+
+class LinkedList:
+	def __init__(self):
+		self.head=None
+
+if __name__=='__main__':
+	llist=LinkedList()
+	llist.head=Node(1);
+	second=Node(2);
+	third=Node(3);
+	llist.head.next=second;
+	second.next=third;
+	while(llist.head!=None):
+		print llist.head.data;
+		llist.head=llist.head.next;
