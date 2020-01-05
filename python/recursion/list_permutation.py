ac33c43ff7d2252626abb081a3cb75f357cba9c1
shinelikeasmile <49489706+shinelikeasmile@users.noreply.github.com>
2020-01-05 11:52:47 +0530

    Create list_permutation.py

---
+++ python/recursion/list_permutation.py
@@ -0,0 +1,44 @@
+import copy
+def permute(input_list):
+	if len(input_list)<=1:
+		return [input_list]
+	elif len(input_list)==2:
+		return [input_list,input_list[::-1]]
+	else :
+		output=[]
+		element=input_list[0]
+		permuted_list=permute(input_list[1:])
+		for item in permuted_list:
+			for i in range(len(item)+1):
+				temp_list=item[:i]+[element]+item[i:]
+				output.append(temp_list)
+		return output
+def check_output(output, expected_output):
+    """
+    Return True if output and expected_output
+    contains the same lists, False otherwise.
+    
+    Note that the ordering of the list is not important.
+    
+    Examples:
+        check_output([ [0, 1], [1, 0] ] ], [ [1, 0], [0, 1] ]) returns True
+
+    Args:
+        output(list): list of list
+        expected_output(list): list of list
+    
+    Returns:
+        bool
+    """
+    o = copy.deepcopy(output)  # so that we don't mutate input
+    e = copy.deepcopy(expected_output)  # so that we don't mutate input
+    
+    o.sort()
+    e.sort()
+    return o == e
+
+print ("Pass" if  (check_output(permute([]), [[]])) else "Fail")
+print ("Pass" if  (check_output(permute([0]), [[0]])) else "Fail")
+print ("Pass" if  (check_output(permute([0, 1]), [[0, 1], [1, 0]])) else "Fail")
+print ("Pass" if  (check_output(permute([0, 1, 2]), [[0, 1, 2], [0, 2, 1], [1, 0, 2], [1, 2, 0], [2, 0, 1], [2, 1, 0]])) else "Fail")
+				
