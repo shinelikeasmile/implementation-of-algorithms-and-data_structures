a3c710096ce9d53a1d44ae82fafe8e1f458117cf
shinelikeasmile <49489706+shinelikeasmile@users.noreply.github.com>
2020-03-26 21:27:37 +0530

    Create Kth_smallest_quick_select.py

---
+++ python/sortings/Kth_smallest_quick_select.py
@@ -0,0 +1,37 @@
+def partition (array,low,high):
+	pivot=array[high]
+	i=low
+	j=low
+	while(j<high):
+		if(array[j]>pivot):
+			pass
+		else:
+			temp=array[j]
+			array[j]=array[i]
+			array[i]=temp
+			i+=1
+		j+=1
+	temp=array[i]
+	array[i]=pivot
+	array[high]=temp
+	return i
+	
+def quick_sort(array,low,high,K):
+	if(low<high):
+		pivot=partition(array,low,high)
+		if(pivot==K):
+			return array[K]
+		if(pivot>K):
+			quick_sort(array,low,pivot-1,K)
+		else:
+			quick_sort(array,pivot+1,high,K)
+
+if __name__=="__main__":
+	print("Enter size of the array : ")
+	N=int(input())
+	print("Enter elements of the array : ")
+	L=list(map(int,input().split()))
+	print("Enter K : ")
+	K=int(input())
+	print(quick_sort(L,0,N-1,K-1))
+	
